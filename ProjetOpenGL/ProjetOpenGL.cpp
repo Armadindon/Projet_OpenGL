a9ff5d8f687b7a0a448407f840ce43687bac6dd3
Armadindon <baptisteperrin77@gmail.com>
2022-06-14 18:30:58 +0200

    Resize Fenetre

--- ProjetOpenGL/ProjetOpenGL.cpp
+++ ProjetOpenGL/ProjetOpenGL.cpp
@@ -128,7 +128,7 @@ int main(void)
 		return -1;
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Projet OpenGL", NULL, NULL);
+	window = glfwCreateWindow(1280, 720, "Projet OpenGL", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
