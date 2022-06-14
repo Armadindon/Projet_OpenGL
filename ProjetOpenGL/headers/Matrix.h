aad50accb734297505ea079b9e7813f654305d89
Armadindon <baptisteperrin77@gmail.com>
2022-06-14 19:36:29 +0200

    Corrections sur camera

--- ProjetOpenGL/headers/Matrix.h
+++ ProjetOpenGL/headers/Matrix.h
@@ -8,5 +8,5 @@ float* getTranslationMatrix(float translationX, float translationY, float transl
 float* getRotationMatrix(float Rx, float Ry, float Rz, float theta);
 float* getScaleMatrix(float scaleX, float scaleY, float scaleZ);
 float* getProjectionMatrix(float zNear, float zFar, float aspect, float f);
-float* getLookAtMatrix(vec3 right, vec3 up, vec3 direction, vec3 position);
+float* getLookAtMatrix(vec3 cameraPos, vec3 cameraTarget, vec3 up);
 #endif
